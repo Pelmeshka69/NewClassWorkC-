ed656974657d4ee89976eecd222a4b4955a83373
Pelmeshka69 <133804012+Pelmeshka69@users.noreply.github.com>
2024-01-24 23:30:20 +0200

    23.01.24(3)

---
+++ Program(3).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._01._24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int[] masiv = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                masiv[i] = int.Parse(input[i].ToString());
+            }
+            Console.Write("Масив розрядів: ");
+            for (int i = 0; i < masiv.Length; i++)
+            {
+                Console.Write(masiv[i]);
+                if (i < masiv.Length - 1)
+                {
+                    Console.Write(",");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
